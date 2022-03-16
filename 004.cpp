f979d7f3f996e4836b9fb1ee035cacc0eee9a059
zawakasutera <zawazawanomorichan@outlook.jp>
2022-03-16 23:32:08 +0900

    004

---
+++ 004.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+// 004 カラオケ
+// https://atcoder.jp/contests/pakencamp-2019-day3/tasks/pakencamp_2019_day3_c
+
+// ブルートフォース!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
+
+using namespace std;
+#define all(a) a.begin(), a.end()
+#define int long long
+#define over_load_(_1, _2, _3, _4, NAME, ...) NAME
+#define rep(...) over_load_(__VA_ARGS__, rep4, rep3, rep2)(__VA_ARGS__)
+#define rep2(i, r) for (int i = 0; i < static_cast<int>(r); (i) += 1)
+#define rep3(i, l, r) for (int i = static_cast<int>(l); i < static_cast<int>(r); (i) += 1)
+#define rep4(i, l, r, stride) for (int i = static_cast<int>(l); i < static_cast<int>(r); (i) += (stride))
+#define rrep(...) over_load_(__VA_ARGS__, rrep4, rrep3, rrep2)(__VA_ARGS__)
+#define rrep2(i, r) for (int i = static_cast<int>(r) - 1; i >= 0; (i) -= 1)
+#define rrep3(i, l, r) for (int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= 1)
+#define rrep4(i, l, r, stride) for (int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= (stride))
+#define fore(i, a) for (auto &i : a)
+template <typename T1, typename T2>
+inline bool chmax(T1 &a, T2 b)
+{
+    return a < b && (a = b, true);
+}
+template <typename T1, typename T2>
+inline bool chmin(T1 &a, T2 b)
+{
+    return a > b and (a = b, true);
+}
+constexpr char newl = '\n';
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vp = vector<pair<int, int>>;
+using vs = vector<string>;
+
+signed main()
+{
+    int n, m;
+    cin >> n >> m;
+    vvi a(n, vi(m));
+    fore(y, a) fore(x, y) cin >> x;
+    ll ans = 0;
+    rep(i, m) rep(j, i + 1, m)
+    {
+        ll count = 0;
+        rep(k, n) count += max(a[k][i], a[k][j]);
+        chmax(ans, count);
+    }
+    cout << ans << newl;
+}
