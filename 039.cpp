ea5dc4d5ba66411a2b0da8d8ce913dc682688773
zawakasutera <zawazawanomorichan@outlook.jp>
2022-03-27 16:47:14 +0900

    039

---
+++ 039.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+// 039 １年生(A First Grader)
+// https://atcoder.jp/contests/joi2011yo/tasks/joi2011yo_d
+
+// clang-format off
+
+using namespace std;
+#define all(a) a.begin(), a.end()
+#define int long long
+#define over_load_(_1, _2, _3, _4, NAME, ...) NAME
+#define rep(...) over_load_(__VA_ARGS__, rep4, rep3, rep2)(__VA_ARGS__)
+#define rep2(i, r) for (int i = 0; i < static_cast<int>(r); (i) += 1)
+#define rep3(i, l, r) for (int i = static_cast<int>(l); i < static_cast<int>(r); (i) += 1)
+#define rep4(i, l, r, stride) for (int i = static_cast<int>(l); i < static_cast<int>(r); (i) += (stride))
+#define rrep(...) over_load_(__VA_ARGS__, rrep4, rrep3, rrep2)(__VA_ARGS__)
+#define rrep2(i, r) for (int i = static_cast<int>(r) - 1; i >= 0; (i) -= 1)
+#define rrep3(i, l, r) for (int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= 1)
+#define rrep4(i, l, r, stride) for (int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= (stride))
+#define fore(i, a) for (auto &i : a)
+template <typename T1, typename T2>
+inline bool chmax(T1 &a, T2 b)
+{
+    return a < b && (a = b, true);
+}
+template <typename T1, typename T2>
+inline bool chmin(T1 &a, T2 b)
+{
+    return a > b and (a = b, true);
+}
+constexpr char newl = '\n';
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vp = vector<pair<int, int>>;
+using vs = vector<string>;
+
+signed main() {
+    int n; cin >> n;
+    vi nums(n - 1); fore(x, nums) cin >> x;
+    int last; cin >> last;
+
+    vvi dp(n - 1, vi(21, 0));
+    dp[0][nums[0]] = 1;
+
+    rep(i, n - 2) rep(j, 21) {
+        if (dp[i][j] == 0) continue;
+        if (j + nums[i + 1] <= 20) dp[i + 1][j + nums[i + 1]] += dp[i][j];
+        if (j - nums[i + 1] >= 0) dp[i + 1][j - nums[i + 1]] += dp[i][j];
+    }
+
+    cout << dp[n - 2][last] << newl;
+}
