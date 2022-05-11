77b5a3ed47d9ef8f42764302219f305b29c788ad
zawakasutera <zawazawanomorichan@outlook.jp>
2022-05-11 18:15:47 +0900

    073

---
+++ 073.cpp
@@ -0,0 +1,78 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+
+// 073 Kinght
+
+// clang-format off
+
+using namespace std;
+#define all(a) a.begin(), a.end()
+#define int long long
+#define over_load_(_1, _2, _3, _4, NAME, ...) NAME
+#define rep(...) over_load_(__VA_ARGS__, rep4, rep3, rep2)(__VA_ARGS__)
+#define rep2(i, r) for (int i = 0; i < static_cast<int>(r); (i) += 1)
+#define rep3(i, l, r) for (int i = static_cast<int>(l); i < static_cast<int>(r); (i) += 1)
+#define rep4(i, l, r, stride) for (int i = static_cast<int>(l); i < static_cast<int>(r); (i) += (stride))
+#define rrep(...) over_load_(__VA_ARGS__, rrep4, rrep3, rrep2)(__VA_ARGS__)
+#define rrep2(i, r) for (int i = static_cast<int>(r) - 1; i >= 0; (i) -= 1)
+#define rrep3(i, l, r) for (int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= 1)
+#define rrep4(i, l, r, stride) for (int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= (stride))
+#define fore(i, a) for (auto &i : a)
+template <typename T1, typename T2>
+inline bool chmax(T1 &a, T2 b)
+{
+    return a < b && (a = b, true);
+}
+template <typename T1, typename T2>
+inline bool chmin(T1 &a, T2 b)
+{
+    return a > b and (a = b, true);
+}
+constexpr char newl = '\n';
+
+long long pow2(int x) {return x * x;}
+bool isupper(char c) {return ('A' <= c and c <= 'Z');}
+bool islower(char c) {return ('a' <= c and c <= 'z');}
+bool isnumber(char c) {return ('0' <= c and c <= '9');}
+
+// O(logX)
+int getdigit(long long x) {
+    int res = 0;
+    while(x > 0) {
+        res++;
+        x /= 10;
+    }
+    return res;
+}
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+using vp = vector<pair<int, int>>;
+using vs = vector<string>;
+
+const ll sup = LONG_LONG_MAX / 2 - 1;
+const double pi = 3.141592653589793;
+
+using namespace atcoder;
+using mint = modint1000000007;
+
+mint comb(int n, int r) {
+    mint a = mint::raw(1);
+    rep(i, r) a *= n - i;
+    rep(i, 1, r + 1) a /= i;
+    return a; 
+}
+
+signed main() {
+    int x, y; cin >> x >> y;
+    bool ok = true;
+    if ((x + y) % 3 != 0) ok = false;
+    if (!((x + 1) / 2 <= y and y <= 2 * x)) ok = false;
+
+    int n = (x + y) / 3;
+    if (ok) cout << comb(n, y - n).val() << newl;
+    else cout << 0 << newl;
+}
